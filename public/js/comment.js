31bd236c596d85e8994fed85f7fd23c8b5a901dc
mshaari <michael.shaari@gmail.com>
2023-02-21 12:33:30 -0500

    Changes to views and commenting functionality

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,22 +3,22 @@ const projectComment = document.getElementById("projectComment");
 const submitButton = document.getElementById("submitComment");
 
 // "e" is the argument in the function, followed by the block of code to run
-submitButton?.addEventListener("click", async e => {
-    e.preventDefault();
+submitButton.addEventListener("click", async (event) => {
+    event.preventDefault();
     const body = projectComment.value;
 
     if (body) {
-        const newComment = await fetch('/api/comments/', {
+        const newComment = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({ body }),
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json', },
         })
 
         if (newComment.ok) {
             // If successful, reload page with new comment
             window.location.reload();
           } else {
-            alert(response.statusText);
+            alert('Failed to create comment');
         };
     };
 });
